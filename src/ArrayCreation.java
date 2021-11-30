9b42d58c2417cd10e392f3741c8c8d105e30a045
AveryCohen <AveryCohen04@gmail.com>
2021-11-30 10:28:15 -0500

    worked on array creation

--- src/ArrayCreation.java
+++ src/ArrayCreation.java
@@ -1,26 +1,43 @@
 public class ArrayCreation {
 
     public static double getAverage(double[] numbers) {
-
-       return 0.0; // Make sure to update this line. It is a temporary placeholder to avoid an error.
+        if (numbers.length >= 3) {
+            double unrounded = (numbers[0] + ((numbers.length-1)/2) + numbers.length-1) / 3;
+            double rounded= (int)(unrounded+0.5);
+            return rounded;
+        }
+        else {
+            return 0.0;
+        }
     }
+
 
     public static String stringArray(String[] friends) {
 
-        return ""; // Make sure to update this line. It is a temporary placeholder to avoid an error.
+        if (friends.length == 5) {
+            int lOne = friends[0].length() ;
+            int lTwo = friends[1].length() ;
+            int lThree = friends[2].length() ;
+            int lFour = friends[3].length() ;
+            int lFive = friends[4].length() ;
+            return "" + lOne + " " + lTwo + " " + lThree + " " + lFour + " " + lFive;
+        }
+        else {
+            return "";
+        }
     }
+
 
     public static String countryData() {
         String[] countries = {"China", "Egypt", "France", "South Korea", "Germany", "India", "Japan", "Ghana",
                 "Kenya", "Mexico", "United Kingdom", "Burkina Faso", "United States"};
 
         String[] capitals = {"Beijing", "Cairo", "Paris", "Seoul", "Berlin", "New Delhi", "Tokyo", "Accra", "Nairobi",
-        "Mexico City", "London", "Ouagadougou", "Washington D.C."};
+                "Mexico City", "London", "Ouagadougou", "Washington D.C."};
 
         String[] languages = {"Mandarin", "Arabic", "French", "Korean", "German", "Hindi", "Japanese", "Akuapem Twi",
-        "Swahili", "Spanish", "English", "French", "English"};
-
-
-        return "";
+                "Swahili", "Spanish", "English", "French", "English"};
+        int data= (int)(Math.random()*13);
+        return "The capital of "+countries[data]+" is "+capitals[data]+" and the primary language is "+languages[data];
     }
 }
