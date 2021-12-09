7bf689d13e1174f94c196100275ccc9b01b7cc47
AveryCohen <AveryCohen04@gmail.com>
2021-12-09 11:55:59 -0500

    started spell check

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,10 +1,11 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class SpellChecker {
 
-    public static String[] read() throws IOException {
+    public static String[] getDictionary() throws IOException {
         String [] arr = new String[61336];
         Scanner scan = new Scanner(new File("files/words.txt"));
         int count = 0;
@@ -12,18 +13,16 @@ public class SpellChecker {
             arr[count] = scan.next();
             count++;
         }
-
         return arr;
     }
-
-        public static int search(String[] arr, String word) {
+        public static int wordInDictionary(String word, String [] dictionary) {
             int min = 0;
-            int max = arr.length - 1;
+            int max = dictionary.length - 1;
             while (min <= max) {
                 int mid = (min + max) / 2;
-                if (arr[mid].compareTo(word) == 0)
+                if (dictionary[mid].compareTo(word) == 0)
                     return mid;
-                else if (arr[mid].compareTo(word) > 0)
+                else if (dictionary[mid].compareTo(word) > 0)
                     max = mid + 1;
                 else
                     min = mid - 1;
@@ -31,7 +30,31 @@ public class SpellChecker {
             return -1;
         }
     public static void main(String [] args) throws IOException {
-        String[] arr = read();
+        String[] arr = getDictionary();
+        String currentWord = new String;
+        Scanner mainScan = new Scanner(System.in);
+        Scanner scanAlice = new Scanner(new File("files/alice.txt"));
+        FileWriter newAlice = new FileWriter("files/corrected.txt");
+        while (scanAlice.hasNext()) {
+            currentWord = scanAlice.next();
+            if (wordInDictionary(currentWord, arr) == -1) {
+                System.out.println("The word: " + currentWord + "may be spelled incorrectly, would you like to change it? Please type y or n");
+                String answer = mainScan.nextLine();
+                if (answer.equals("y")) {
+                    System.out.println("Please enter in the correct spelling: ");
+                    newAlice.write(mainScan.nextLine() + " ");
+                }
+                else
+                {
+                    newAlice.write(scanAlice.next() + " ");
+                }
+            }
+            else
+            {
+                newAlice.write(scanAlice.next() + " ");
+            }
+        }
+        newAlice.close();
     }
 
 
