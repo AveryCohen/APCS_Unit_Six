b1e086f3175c0f001f0adab38e46ecb19f1c0e9d
AveryCohen <AveryCohen04@gmail.com>
2021-12-10 14:02:10 -0500

    Finished Spell Check

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -20,25 +20,25 @@ public class SpellChecker {
             int max = dictionary.length - 1;
             while (min <= max) {
                 int mid = (min + max) / 2;
-                if (dictionary[mid].compareTo(word) == 0)
-                    return mid;
-                else if (dictionary[mid].compareTo(word) > 0)
-                    max = mid + 1;
+                if (dictionary[mid].compareToIgnoreCase(word) == 0)
+                    return 0;
+                else if (dictionary[mid].compareToIgnoreCase(word) > 0)
+                    max = mid - 1;
                 else
-                    min = mid - 1;
+                    min = mid + 1;
             }
             return -1;
         }
     public static void main(String [] args) throws IOException {
         String[] arr = getDictionary();
-        String currentWord = new String;
+        String currentWord;
         Scanner mainScan = new Scanner(System.in);
         Scanner scanAlice = new Scanner(new File("files/alice.txt"));
         FileWriter newAlice = new FileWriter("files/corrected.txt");
         while (scanAlice.hasNext()) {
             currentWord = scanAlice.next();
             if (wordInDictionary(currentWord, arr) == -1) {
-                System.out.println("The word: " + currentWord + "may be spelled incorrectly, would you like to change it? Please type y or n");
+                System.out.println("The word: " + currentWord + " may be spelled incorrectly, would you like to change it? Please type y or n");
                 String answer = mainScan.nextLine();
                 if (answer.equals("y")) {
                     System.out.println("Please enter in the correct spelling: ");
@@ -46,12 +46,12 @@ public class SpellChecker {
                 }
                 else
                 {
-                    newAlice.write(scanAlice.next() + " ");
+                    newAlice.write(currentWord + " ");
                 }
             }
             else
             {
-                newAlice.write(scanAlice.next() + " ");
+                newAlice.write(currentWord + " ");
             }
         }
         newAlice.close();
